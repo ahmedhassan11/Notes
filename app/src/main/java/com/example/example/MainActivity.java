26ab35f2bab1dee04febb20a55fed720f5f95909
ahmed <amed88616@gmail.com>
2020-02-26 17:12:36 +0200

    initial commit

--- app/src/main/java/com/example/example/MainActivity.java
+++ app/src/main/java/com/example/example/MainActivity.java
@@ -134,6 +134,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+
         MenuInflater menuInflater = getMenuInflater();
         menuInflater.inflate(R.menu.main_menu, menu);
         return super.onCreateOptionsMenu(menu);
